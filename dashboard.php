50370b0aaead75cb486096968cc4163a8979692e
Azaanul <if23.azaanulramadhan@mhs.ubpkarawang.ac.id>
2024-11-13 22:29:06 +0700

    Update and rename dashboard.php to profile.php

--- dashboard.php
+++
@@ -1,5 +0,0 @@
-<h1>Selamat Datang, Admin!</h1>
-<div class="mt-4">
-    <a href="#" class="btn btn-primary mr-2">Kelola Pengguna</a>
-    <a href="index.php" class="btn btn-danger">Logout</a>
-</div>
